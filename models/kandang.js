126de4b9744c1c7ab88410a43fabe98388162c83
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-04 15:14:39 +0700

    UCP1

---
+++ models/kandang.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Kandang = sequelize.define("Kandang", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Nama_hewan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Nama_petugas: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Usia_hewan: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Jenis_hewan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Tahun_lahir: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
+  }, {
+    tableName: "Kandang",
+    timestamps: false,
+    freezeTableName: true
+  });
+  return Kandang;
+}
